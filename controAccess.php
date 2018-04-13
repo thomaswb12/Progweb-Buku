f09eccafc980bb96ba518296b90e7a365c4aac09
thomaswb12 <36703617+thomaswb12@users.noreply.github.com>
2018-04-13 17:34:24 +0700

    penambahan control access
    php control acces

--- controAccess.php
+++ controAccess.php
@@ -6,10 +6,31 @@
             header("location:");
         }
         else{
-            if($_POST['id'] == "" || $_POST['password'] == ""){
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "progweb";
+
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            } 
+            
+            $sql =  " SELECT * FROM karyawan where idKaryawan = '".$_POST['id']."' and pass = '".$_POST['password']."'";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                echo "success";
+            } 
+            else {
+                echo "0 results";
+            }
+            /*if($_POST['id'] == "" || $_POST['password'] == ""){
                 $_SESSION['error'] = 1;
                 header("location:index.php");
-            }
+            }*/
         }
     }
 ?>
