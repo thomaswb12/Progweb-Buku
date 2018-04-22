b79f6159e2bd0652983a4a47aa7b71039171e13a
thomaswb12 <36703617+thomaswb12@users.noreply.github.com>
2018-04-22 16:33:44 +0700

    Update user secure
    user secure

---
+++ getDataBuku.php
@@ -0,0 +1,43 @@
+<?php
+    if(isset($_SESSION['id'])){
+        require("koneksi.php");
+
+
+        function getBuku(){
+            global $conn;
+            $sql =  " SELECT * FROM buku";
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0) {
+                while($rows = $result->fetch_assoc()){
+                    $data[] = array('judulBuku'=>$rows['judulBuku'],'Dipinjam'=>$rows['Dipinjam']);
+                    
+                }
+            }
+            else{
+                echo "gagal";
+            } 
+            $conn->close();
+            return $data;
+        }
+        function jumlahEksemplar(){
+            global $conn;
+            $sql =  " SELECT * FROM eksbuku";
+            $result = $conn->query($sql);
+            if ($result->num_rows > 0) {
+                while($rows = $result->fetch_assoc()){
+                    $data[] = array('judulBuku'=>$rows['judulBuku'],'Dipinjam'=>$rows['Dipinjam']);
+                    
+                }
+            }
+            else{
+                echo "gagal";
+            } 
+            $conn->close();
+            return $data;
+        }
+    }
+    else{
+        header("location:index.php");
+    }
+    
+?>
