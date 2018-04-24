14a329b8594a745ff40a285c54fd2ad928f9a1ec
thomaswb12 <36703617+thomaswb12@users.noreply.github.com>
2018-04-24 20:45:32 +0700

    contoh template kasir

--- kasir/KasirDefault.php
+++ kasir/KasirDefault.php
@@ -11,6 +11,7 @@
         <title>Kasir - Peminjaman</title>
         <link href="Kasir - peminjaman/Kasir - peminjaman.css" type="text/css" rel="stylesheet">
     </div>
+    <link rel="stylesheet" href="KasirDefault.css" type="text/css">
     <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.1.min.js"></script>
     <script src="KasirDefault.js"></script>
     <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
