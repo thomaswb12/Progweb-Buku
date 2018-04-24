34d083d6f5a0bb01a3beee518d637c905dbcdf63
thomaswb12 <36703617+thomaswb12@users.noreply.github.com>
2018-04-24 22:20:23 +0700

    Perbaikan kasir pengembalian

---
+++ kasir/Kasir - pengembalian/KontenKasirPengembalian.php
@@ -0,0 +1,62 @@
+<h1>Pengembalian</h1>
+<div>
+    <form action="">
+        <div>
+            <i class="fas fa-user floatKiri margin" style="display: block; float:left; font-size: 100px; color: rgba(94, 94, 94, 0.9)"></i>
+            <div>
+                <label for="">ID Member</label>
+                <br>
+                <input type="text"><br>
+                <label for="">Nama</label><br>
+                <input type="text">
+            </div>
+        </div>
+    </form>
+</div>
+<div class="center">
+    <table class="center">
+        <thead>
+            <th>ID Buku</th>
+            <th>Judul Buku</th>
+            <th>Tanggal Pinjam</th>
+            <th>Tanggal Pengembalian</th>
+            <th>Telat</th>
+            <th>Denda</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1234567788</td>
+                <td>Naruto</td>
+                <td>01/01/2018</td>
+                <td>01/01/2018</td>
+                <td>-</td>
+                <td>Rp.0</td>
+            </tr>
+            <tr>
+                <td>1234567788</td>
+                <td>Naruto</td>
+                <td>01/01/2018</td>
+                <td>01/01/2018</td>
+                <td>-</td>
+                <td>Rp.0</td>
+            </tr>
+            <tr>
+                <td>1234567788</td>
+                <td>Naruto</td>
+                <td>01/01/2018</td>
+                <td>01/01/2018</td>
+                <td>-</td>
+                <td>Rp.0</td>
+            </tr>
+        </tbody>
+        <tfoot>
+            <tr>
+                <td class="tebal"colspan="5">Total</td>
+                <td>Rp.0</td>
+            </tr>
+        </tfoot>
+    </table>
+</div>
+<div>
+    <input class="tebal tombol floatKanan"type="button" value="Print Nota">
+</div>
