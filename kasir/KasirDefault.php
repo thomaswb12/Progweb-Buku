4b6202eb411cfd41355e938741599270c7cfe0d1
thomaswb12 <36703617+thomaswb12@users.noreply.github.com>
2018-04-24 10:29:40 +0700

    Kasir sudah di buat ajax

--- kasir/KasirDefault.php
+++ kasir/KasirDefault.php
@@ -4,30 +4,28 @@
     <?php 
         session_start();
     ?>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Template</title>
-    <link href="Kasir - peminjaman.css" type="text/css" rel="stylesheet">
+    <div id="gantiHead">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Kasir - Peminjaman</title>
+        <link href="Kasir - peminjaman/Kasir - peminjaman.css" type="text/css" rel="stylesheet">
+    </div>
     <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.1.min.js"></script>
-    <script src="Kasir - peminjaman.js"></script>
+    <script src="KasirDefault.js"></script>
     <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
 </head>
 <body>
     <header>
         <?php
-            include "../Template/header.php";
+            include "Template/header.php";
         ?>
     </header>
     <article>
         <?php 
-            include "../Template/aside.php"
+            include "Template/aside.php"
         ?>
         <div id="konten">
-            <p id="cek">1</p>
-            <?php 
-                $data; 
-            ?>
             <h1>Peminjaman</h1>
             <div>
                 <form action="">
--- kasir/Template/header.php
+++ kasir/Template/header.php
@@ -1,5 +1,5 @@
     <div>
-        <img src="../../logobaru.png" id="logo"/>
+        <img src="../logobaru.png" id="logo"/>
         <p class="blue font15" id="welcome">Welcome, <span id="namaUser"> <?php echo $_SESSION['nama'];?></span> | <span id="logOut">Log Out</span></p>
         <p id="tanggal" class="blue font15">tanggal</p>    
     </div>
