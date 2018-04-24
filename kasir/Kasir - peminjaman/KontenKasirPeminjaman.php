4aa5dc8851775ca4f61b23f98246e5bb77047f6b
thomaswb12 <36703617+thomaswb12@users.noreply.github.com>
2018-04-24 19:06:02 +0700

    perbaikan kasirPeminjaman

--- kasir/Kasir - peminjaman/KontenKasirPeminjaman.php
+++ kasir/Kasir - peminjaman/KontenKasirPeminjaman.php
@@ -1,9 +1,15 @@
 <h1>Peminjaman</h1>
-<div>
+<div id="pinggir">
     <form action="">
-        <div>
-            <input type="text">
-            <input type="text">
+        <div class="">
+            <i class="fas fa-user" style="display: block; float:left; font-size: 100px;"></i>
+            <div >
+                <label for="">ID Member</label>
+                <br>
+                <input type="text"><br>
+                <label for="">Nama</label><br>
+                <input type="text">
+            </div>
         </div>
         <div>
             <input type="text" name="" id="">
