29331a8042e15ff1f397cf9b2a1acfd1457ca108
thomaswb12 <36703617+thomaswb12@users.noreply.github.com>
2018-04-25 22:28:53 +0700

    perubahan di daftar member

--- kasir/Kasir - daftar member/KontenKasirDaftarMember.php
+++ kasir/Kasir - daftar member/KontenKasirDaftarMember.php
@@ -19,24 +19,14 @@
             <th class="nomor">No</th>
             <th class="idMember">ID Member</th>
             <th class="namaMember">Nama Member</th>
-            <th class="nomorIdentitas">No. Identitas</th>
-            <th class="gender">L/P</th>
-            <th class="tanggalLahir">Tgl Lahir</th>
             <th class="email">Email</th>
-            <th class="nomorTelepon">No Telepon</th>
-            <th class="alamat">Alamat</th>
         </thead>
         <tbody>
-            <tr>
+            <tr onclick="pencot()">
                 <td class="nomor">1</td>
                 <td class="idMember">1234567890</td>
                 <td class="namaMember">Namanya siapa</td>
-                <td class="nomorIdentitas">1234567890</td>
-                <td class="gender">P</td>
-                <td class="tanggalLahir">01/01/2018</td>
                 <td class="email">email@gmail.com</td>
-                <td class="nomorTelepon">081234567890</td>
-                <td class="alamat">Jl. Alamat No 100</td>
             </tr>
         </tbody>
     </table>
@@ -46,5 +36,6 @@
 
 <div id="blur">
 </div>
-<div id="popup">
+<div id="popup" > apapun
 </div>
+<script src="Kasir - daftar member/kasirdaftarmember.js"></script>
