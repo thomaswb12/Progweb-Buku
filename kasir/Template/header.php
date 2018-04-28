977f69966e20fb06accc4558ba334c24dcb0cb31
thomaswb12 <36703617+thomaswb12@users.noreply.github.com>
2018-04-28 21:56:21 +0700

    penmabahan href logout ke index.php

--- kasir/Template/header.php
+++ kasir/Template/header.php
@@ -1,6 +1,6 @@
     <div>
         <img src="../logobaru.png" id="logo"/>
-        <p class="blue font15" id="welcome">Welcome, <span id="namaUser"> <?php echo $_SESSION['nama'];?></span> | <span id="logOut">Log Out</span></p>
+        <p class="blue font15" id="welcome">Welcome, <span id="namaUser"> <?php echo $_SESSION['nama'];?></span> |  <a href="../"><span id="logOut">Log Out</span></a></p>
         <p id="tanggal" class="blue font15">tanggal</p>    
     </div>
 
