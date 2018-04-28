87aea547c18be1d81fcc3ca520cd0a3a7c9a00f3
thomaswb12 <36703617+thomaswb12@users.noreply.github.com>
2018-04-28 11:46:31 +0700

    pembuatan template

---
+++ Manager/template/header.php
@@ -0,0 +1,3 @@
+<img src="../logobaru.png" id="logo"/>
+<p class="blue font15" id="welcome">Welcome, <span id="namaUser">Erinda Resha</span> | <span id="logOut">Log Out</span></p>
+<p id="tanggal" class="blue font15">tanggal</p>
