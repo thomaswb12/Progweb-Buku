cc0160a020652aee7161734d69abdd852dabead8
sylviaputri <sylvi.bluemochi@gmail.com>
2018-05-03 21:01:26 +0700

    revisi tanggal pake php (kurang di user)

--- Gudang/gudangDefault.js
+++ Gudang/gudangDefault.js
@@ -1,7 +1,4 @@
 $(document).ready(function(){
-    //------ tampilkan tanggal NOW -------------------
-    setTanggal(); //panggil fungsi setTanggal()
-
     //------ kalau discroll, panggil fungsi backToTop(), utk membuat tombol UP muncul -------------
     $(window).on('scroll', function () {
         backToTop();
@@ -224,15 +221,4 @@ function backToTop(){
     } else { //bila user belum scroll jauh, tombol disembunyikan
         $('#tombolUp').css('display','none');
     }
-}
-
-//----------- fungsi utk memasukkan tanggal NOW ke p#tanggal di header ----------
-function setTanggal(){ 
-    var n =  new Date();
-    var day = new Array("Minggu","Senin","Selasa","Rabu","Kamis","Jum'at","Sabtu");
-    var date = n.getDate();
-    var month = new Array("Januari", "Februari", "Maret", "April", "Mei", "Agustus", "September", "Oktober", "November", "Desember");
-    var year = n.getFullYear();
-    //tampilkan dalam format DAY,DATE MONTH YEAR
-    $('p#tanggal').text(day[n.getDay()] + ", " + date + " " + month[n.getMonth() + 1] + " " + year);
 }
