af4402cbb82795434c52013f566fba963a807d40
sylviaputri <sylvi.bluemochi@gmail.com>
2018-05-06 16:32:24 +0700

    tambah file manager.php di fungsi

---
+++ functionPHP/manager.php
@@ -0,0 +1,13 @@
+<?php
+    require "koneksi.php";
+
+    //ambil semua daftar peminjaman di tabel transaksi
+    function getAllPeminjaman(){
+        
+    }
+
+    //ambil daftar peminjaman yg di search
+    function getAllPeminjamanWith($kata,$dari){
+        
+    }
+?>
