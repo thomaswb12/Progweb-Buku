6e6c2438ffa8da6363c333777a394d63a6e51747
sylviaputri <sylvi.bluemochi@gmail.com>
2018-05-15 02:39:39 +0700

    update manager daftar peminjaman
    belum fix

--- functionPHP/isiKontenDaftarPeminjaman.php
+++ functionPHP/isiKontenDaftarPeminjaman.php
@@ -2,16 +2,18 @@
      include "manager.php";
      require "koneksi.php";
 
-//     if(isset($_POST['keyword'])&&isset($_POST['searchby']))
-//        $hasil = getAllPeminjamanWith($_POST['keyword'],$_POST['searchby']);
-//     else
-//        $hasil = getAllPeminjaman();
-//    foreach($hasil as $pinjam){
+     if(isset($_POST['keyword'])&&isset($_POST['searchby'])){
+        $hasil = getAllPeminjamanWith($_POST['keyword'],$_POST['searchby']);
+     }
+     else{
+        $hasil = getAllPeminjaman();
+     }
+    foreach($hasil as $pinjam){
         echo    '<tr onclick="aside2()">
-                    <td>2343</td>
-                    <td>idid2</td>
-                    <td>nama coba2</td>
+                    <td>'.$pinjam['tanggalAturanKembali'].'</td>
+                    <td>'.$pinjam['id'].'</td>
+                    <td>'.$pinjam['nama'].'</td>
                     <td class="peringatan"><i class="fas fa-exclamation-triangle" style="color:red"></i></td>
                 </tr>';                
-//    }
+    }
 ?>
