f5af50fe007196724127fb1c3ccf0449bdc6f72f
erindaresha <38478927+erindaresha@users.noreply.github.com>
2018-05-21 08:40:02 +0700

    update

--- functionPHP/controAccess.php
+++ functionPHP/controAccess.php
@@ -2,16 +2,16 @@
     //print_r($_POST);
     if($_POST){//untuk mengecek apakah sudah ada yg post
         session_start();
-        if($_POST['id'] == "ADMN0000" && $_POST['password'] == "admin"){ //untuk login ke page user
-            $_SESSION['id'] = $_POST['id'];
-            $_SESSION['control'] = 0;
-            $_SESSION['nama'] = "Admin";
-            header("location:../Admin/AdminDefault.php");break;
-        }
         if($_POST['id'] == "admin" && $_POST['password'] == "admin"){ //untuk login ke page user
             $_SESSION['id'] = $_POST['id'];
             $_SESSION['control'] = 1;
             header("location:../User/UserDaftarKomik/UserDaftarKomik.php");//untuk pindah ke page tersebut
+        }
+        else if($_POST['id'] == "ADMN0000" && $_POST['password'] == "admin"){ //untuk login ke page user
+            $_SESSION['id'] = $_POST['id'];
+            $_SESSION['control'] = 0;
+            $_SESSION['nama'] = "Admin";
+            header("location:../Admin/AdminDefault.php");
         }
         else{
             if($_POST['id'] == "" || $_POST['password'] == ""){
@@ -23,9 +23,6 @@
                 $_SESSION['pass'] = $_POST['password'];
                 require("getIdentitas.php");
                 switch ($_SESSION['idJabatan']){
-                    case "ADMN" :   $_SESSION['id'] = $_POST['id'];
-                                    $_SESSION['control'] = 0;
-                                    header("location:../Admin/AdminDefault.php");break;
                     case "KSRR" :   $_SESSION['id'] = $_POST['id'];
                                     $_SESSION['control'] = 2;
                                     header("location:../kasir/KasirDefault.php");break;
